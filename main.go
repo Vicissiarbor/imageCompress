07c490228ed3ff2b2fcdfeb5c5c6bd79ba95f8c7
Vicissiarbor <qqczfgxybk@petalmail.com>
2026-09-11 19:49:25 +0800

    新增：压缩图片

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 
 	"github.com/gin-gonic/gin"
+	xdraw "golang.org/x/image/draw"
 )
 
 func main(){
@@ -42,10 +43,10 @@ func compressHardler(c *gin.Context){
 		c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
 		return
 	}
-	
-	scaleStr := c.DefaultQuery("scale", "1.0")
+	scale := 1.0	
+	scaleStr := c.Query("scale")
 	if scaleStr != ""{
-		scale, err := strconv.ParseFloat(scaleStr, 64)
+		scale, err = strconv.ParseFloat(scaleStr, 64)
 		if scale <= 0 || scale > 1.0 {
 			err = errors.New("scale out size.")
 		}
@@ -56,6 +57,30 @@ func compressHardler(c *gin.Context){
 	}
 	
 	img, format, err := image.Decode(bytes.NewReader(src))
+	if format == "jpeg"{
+		qualityStr := c.DefaultQuery("scale", "1.0")
+		if qualityStr != ""{
+			quality, err := strconv.ParseFloat(qualityStr, 64)
+			if quality < 1 || quality > 100 {
+				err = errors.New("quality out size.")
+			}
+			if err != nil{
+				c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+				return
+			}
+		}
+	}
+	
+	if scale != 1.0{
+		b := img.Bounds()
+		w := int(float64(b.Dx())*scale)
+		h := int(float64(b.Dy())*scale)
 
+		w = max(w, 1)
+		h = max(h, 1)
 
+		dst := image.NewRGBA(image.Rect(0, 0, w, h))
+		xdraw.CatmullRom.Scale(dst, dst.Bounds(), img, b, xdraw.Src, nil)
+		img = dst
+	}	
 }
